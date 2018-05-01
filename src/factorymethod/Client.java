88ed750d77371e8296a6a1a191359b291464c168
Gaurav Gupta <ggupta@Ban-2ggupta-m.local>
2018-05-01 20:15:46 +0530

    Factory Method Implementation

--- src/factorymethod/Client.java
+++ src/factorymethod/Client.java
@@ -1,4 +1,16 @@
 package factorymethod;
 
 public class Client {
+    public static void main(String str[]) {
+       Mobile android, ios, windows, symbian;
+       android = MobileFactory.getMobile(MobileType.TYPE_ANDROID);
+       ios = MobileFactory.getMobile(MobileType.TYPE_IOS);
+       windows = MobileFactory.getMobile(MobileType.TYPE_WINDOWS);
+       symbian = MobileFactory.getMobile(MobileType.TYPE_SYMBIAN);
+
+       android.printMobile();
+       windows.printMobile();
+       ios.printMobile();
+       symbian.printMobile();
+    }
 }
--- src/factorymethod/MobileFactory.java
+++ src/factorymethod/MobileFactory.java
@@ -3,6 +3,87 @@ package factorymethod;
 /**
  *
  */
-public class FactoryMethod {
+public class MobileFactory {
 
+    public static Mobile getMobile(MobileType mobileType) {
+        switch (mobileType) {
+            case TYPE_ANDROID:
+                return new AndroidMobile();
+            case TYPE_IOS:
+                return new IOSMobile();
+            case TYPE_WINDOWS:
+                return new WindowsMobile();
+            case  TYPE_SYMBIAN:
+                return new SymbianMobile();
+                default:
+                    throw new RuntimeException("Mobile type not supported");
+        }
+    }
+}
+
+abstract class Mobile {
+
+    MobileType mMobileType;
+
+    public Mobile(MobileType mMobileType) {
+        this.mMobileType = mMobileType;
+    }
+
+    /**
+     *   Print mobile type
+     */
+    abstract public void printMobile();
+}
+
+enum MobileType {
+    TYPE_ANDROID, TYPE_IOS, TYPE_WINDOWS, TYPE_SYMBIAN
+}
+
+class AndroidMobile extends Mobile {
+
+    public AndroidMobile() {
+        super(MobileType.TYPE_ANDROID);
+    }
+
+    @Override
+    public void printMobile() {
+        System.out.println("Android Mobile");
+    }
+}
+
+
+class IOSMobile extends Mobile {
+
+    public IOSMobile() {
+        super(MobileType.TYPE_IOS);
+    }
+
+    @Override
+    public void printMobile() {
+        System.out.println("IOS Mobile");
+    }
+}
+
+class WindowsMobile extends Mobile {
+
+    public WindowsMobile() {
+        super(MobileType.TYPE_WINDOWS);
+    }
+
+    @Override
+    public void printMobile() {
+        System.out.println("Windows Mobile");
+    }
+}
+
+class SymbianMobile extends Mobile {
+
+    public SymbianMobile() {
+        super(MobileType.TYPE_SYMBIAN);
+    }
+
+    @Override
+    public void printMobile() {
+        System.out.println("Symbian Mobile");
+    }
 }
